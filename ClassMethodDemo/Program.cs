4a756a9af4d55c1ab36a197d2f30f77f4ac54b3f
Ozan Celik <ozancex@gmail.com>
2022-11-09 22:25:30 +0300

    Add project files.

---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassMethodDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Customer customer1 = new Customer();
+            Customer customer2 = new Customer();
+            Customer customer3 = new Customer();
+
+
+        }
+    }
+}
